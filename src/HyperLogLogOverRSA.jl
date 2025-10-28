6974fde023dbba4175d69c13b04b1f30d190a195
Stefan Karpinski <stefan@karpinski.org>
2025-10-28 12:39:44 -0400

    hash_resource_class: produce UInt64 hash instead of BigInt
    The hash range only needs to cover B*2^m since the random power taken
    later fully randomizes the rest of the behavior.
    
    We could also let the hash do more of the randomization and have t do
    less of the randomization, but this seems eaiser/safer.

--- src/HyperLogLogOverRSA.jl
+++ src/HyperLogLogOverRSA.jl
@@ -6,12 +6,12 @@ include("Ring.jl")
 using SHA
 
 function hash_resource_class(str::AbstractString)
-    h = BigInt()
     bytes = sha224(str)
-    ccall((:__gmpz_import, Base.GMP.libgmp), Cvoid,
-        (Base.GMP.MPZ.mpz_t, Csize_t, Cint, Csize_t, Cint, Csize_t, Ptr{UInt8}),
-        h, length(bytes), 1, 1, 1, 0, bytes)
-        # order=+1, size=1, endian=+1 (big), nails=0
+    h = zero(UInt64)
+    for i = 1:8
+        h <<= 8
+        h |= bytes[i]
+    end
     return h
 end
 
