20f795b7dad22c452d8808e63f8fc3c58d82e257
Stefan Karpinski <stefan@karpinski.org>
2025-12-01 10:48:04 -0500

    Client: different approach to adding noise
    This version is what I've used in the anonymity proof. It's a slightly
    stronger construction since z is fully random and w = z^(B 2^m) so the
    effectiveness of the noise does not depend on powers of g covering
    enough of the ring. This is helpful since a malicious server could pick
    a value of g that isn't actually a semigenerator.
    
    Interestingly, this actually makes message generation _faster_. I think
    the reason is that it avoids exponentiating by any full range values. In
    the new code, exponentiation is done by `h`, which is small (128-bit),
    `B*2^m``, which is also small (128-bit) and simple in the sense of few
    bit set, and `t`, which is also small (128-bit). There are no longer any
    large, random exponents. Instead there's a multiply by `w`, which is a
    large random value, but that's much faster then exponentiation.
    
    New timings: 80µs (on power) 135µs (on battery).

--- src/Client.jl
+++ src/Client.jl
@@ -26,7 +26,7 @@ function Client(cert::RingCert)
         throw(ArgumentError("cert: m ≤ 1: $(cert.m)"))
 
     # check basic modulus properties
-    (N & 3) == 3 ||
+    mod4(N) == 3 ||
         throw(ArgumentError("cert: N ≠ 3 mod 4 (N=$N)"))
     isprime(N >> 1) ||
         throw(ArgumentError("cert: (N-1)/2 not prime (N=$N)"))
@@ -77,11 +77,11 @@ Base.show(io::IO, c::Client) =
 
 function hll_generate(client::Client, class::Any="/registries")
     N, B, m, g, x₀ = client.N, client.B, client.m, client.g, client.x₀
-    B2ᵐ = big(B) << m
-    η = B2ᵐ*rand(rng, 0:(N-1)÷(B2ᵐ))          # η = 0 mod B2^m
     h = hash_resource_class(x₀, class)        # h = H(x₀, class)
-    x = modmul(x₀, powermod(g, h + η, N), N)  # x = x₀ g^(h + η)
-    i = rand(rng, 0:(Int64(1)<<(m-1))-1)      # i ∈ [0, 2^(m-1))
+    x = modmul(x₀, powermod(g, h, N), N)      # x = x₀ g^h
+    z = rand(rng, 1:N-1)                      # z ∈ [1, N)
+    w = powermod(z, widen(B) << m, N)         # w = z^(B 2^m)
+    i = rand(rng, 0:(Int64(1) << (m-1)) - 1)  # i ∈ [0, 2^(m-1))
     t = widemul(2B, i) + 1                    # t = 1 mod 2B
-    y = powermod(x, t, N)                     # y = x^t
+    y = modmul(w, powermod(x, t, N), N)       # y = w x^t
 end
