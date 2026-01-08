355d1906460349b6d84bcdc990cd3f8a1656b66a
Stefan Karpinski <stefan@karpinski.org>
2026-01-08 12:10:56 -0500

    Client: require m ≥ 2

--- src/Client.jl
+++ src/Client.jl
@@ -31,8 +31,8 @@ function Client(cert::RingCert)
         throw(ArgumentError("cert: B even: $B"))
     m ≤ m_max ||
         throw(ArgumentError("cert: m too large: $m"))
-    1 < m ||
-        throw(ArgumentError("cert: m ≤ 1: $m"))
+    m ≥ 2 ||
+        throw(ArgumentError("cert: m < 2: $m"))
 
     # check modulus properties
     Base.top_set_bit(N) ≤ L_max ||
@@ -73,12 +73,12 @@ Base.show(io::IO, c::Client) =
     print(io, "Client(B=$(c.B), m=$(c.m), N=$(c.N), x₀=$(c.x₀))")
 
 function hll_generate(client::Client, class::Any="/registries")
-    N, B, m, g, x₀ = client.N, client.B, client.m, client.g, client.x₀
-    h = hash_resource_class(x₀, class)        # h = H(x₀, class)
-    x = modmul(x₀, powermod(g, h, N), N)      # x = x₀ g^h
-    z = rand(rng, 1:N-1)                      # z ∈ [1, N)
-    w = powermod(z, widen(B) << m, N)         # w = z^(B 2^m)
-    i = rand(rng, 0:(Int64(1) << (m-1)) - 1)  # i ∈ [0, 2^(m-1))
-    t = widemul(2B, i) + 1                    # t = 1 mod 2B
-    y = modmul(w, powermod(x, t, N), N)       # y = w x^t
+    B, m, N, g, x₀ = client.B, client.m, client.N, client.g, client.x₀
+    h = hash_resource_class(x₀, class)      # h = H(x₀, class)
+    x = modmul(x₀, powermod(g, h, N), N)    # x = x₀ g^h
+    z = rand(rng, 1:N-1)                    # z ∈ [1, N)
+    w = powermod(z, widen(B) << m, N)       # w = z^(B 2^m)
+    i = rand(rng, 0:(Int64(1) << (m-1))-1)  # i ∈ [0, 2^(m-1))
+    t = widemul(2B, i) + 1                  # t = 1 mod 2B
+    y = modmul(w, powermod(x, t, N), N)     # y = w x^t
 end
